5468757f983a3f2f9e7e01dbd513fa72b3cc0663
Alice Gregson <gregsonalice1@gmail.com>
2019-11-20 18:29:06 +0000

    edited table and added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  vacancy1 = Vacancy.create(title: "TA", school: "Le Wagon", salary: 500, description: "Teaching Assistant role at Le Wagon Coding Bootcamp")
