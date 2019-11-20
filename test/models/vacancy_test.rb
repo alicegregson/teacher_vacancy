ac305cd95fa3584ec0c9ab79486838421cadefac
Alice Gregson <gregsonalice1@gmail.com>
2019-11-20 06:52:07 +0000

    added 2 model tests and updated error messages on new form

--- test/models/vacancy_test.rb
+++ test/models/vacancy_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class VacancyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "salary returns a number" do
+    vacancy = Vacancy.new(salary: 500)
+    assert_equal 500, vacancy.salary
+  end
+  test "Title returns a string" do
+    vacancy = Vacancy.new(title: "Biology Teacher")
+    assert_equal "Biology Teacher", vacancy.title
+  end
 end
