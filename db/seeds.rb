d507f9fb77e68d82a8772501e0ec058055701d29
Alice Gregson <gregsonalice1@gmail.com>
2019-11-20 23:03:18 +0000

    css styling for show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  puts "Cleaning database..."
+  Vacancy.destroy_all
 
+  puts "Creating vacancies..."
   vacancy1 = Vacancy.create(title: "TA", school: "Le Wagon", salary: 500, description: "Teaching Assistant role at Le Wagon Coding Bootcamp")
+  vacancy2 = Vacancy.create(title: "Biology Teacher", school: "London Bridge Primary", salary: 30000, description: "Open teaching position for a biology teacher at London Bridge Primary")
+
+  puts "Finished!"
