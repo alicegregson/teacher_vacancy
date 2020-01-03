dc5d022c29005489e2fcc063e03860358c0a9fc4
Alice Gregson <gregsonalice1@gmail.com>
2020-01-03 17:06:48 +0000

    added save_and_open_screenshot to job test

--- test/system/jobs_test.rb
+++ test/system/jobs_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class JobsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit "/"
+    save_and_open_screenshot
     assert_selector "h1", text: "Job"
   end
 end
