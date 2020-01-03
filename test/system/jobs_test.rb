b0ed20a322aabfc1f5c24615e48b3123df11af7b
Alice Gregson <gregsonalice1@gmail.com>
2020-01-03 17:26:33 +0000

    added 2nd test to JobsTest for column count

--- test/system/jobs_test.rb
+++ test/system/jobs_test.rb
@@ -3,8 +3,13 @@ require "application_system_test_case"
 class JobsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit "/"
-    save_and_open_screenshot
+    # save_and_open_screenshot
     assert_selector "h1", text: "Job"
     assert_selector ".header-text", count: 1
+  end
+  test "vacancy count" do
+    visit "/"
+    save_and_open_screenshot
+    assert_selector "th", count: 4
   end
 end
